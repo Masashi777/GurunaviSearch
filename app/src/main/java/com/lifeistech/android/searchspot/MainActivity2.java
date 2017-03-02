126f4618fc1f5172287c4bde594b305438d51824
Masashi <hikozaru.kinopio@gmail.com>
2017-03-03 00:36:53 +0900

    Retrofit 2.0

--- app/src/main/java/com/lifeistech/android/searchspot/MainActivity2.java
+++ app/src/main/java/com/lifeistech/android/searchspot/MainActivity2.java
@@ -1,24 +1,19 @@
 package com.lifeistech.android.searchspot;
 
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.lifeistech.android.searchspot.gurunaviAPI.Gurunavi;
 import com.lifeistech.android.searchspot.gurunaviAPI.GurunaviConnect;
 import com.lifeistech.android.searchspot.gurunaviAPI.GurunaviModel.GurunaviResponse;
 import com.lifeistech.android.searchspot.gurunaviAPI.GurunaviModel.Response.apiVersion.Rest;
 import com.lifeistech.android.searchspot.gurunaviAPI.GurunaviRetrofit;
-import com.lifeistech.android.searchspot.gurunaviAPI.GurunaviService;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import retrofit.Call;
 import retrofit.Callback;
@@ -28,44 +23,47 @@ import retrofit.Retrofit;
 
 import static com.lifeistech.android.searchspot.gurunaviAPI.GurunaviConnect.REQUEST_DOMAIN;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
+    static ListView listView;
+    static CustomAdapter adapter;
+    static ArrayList<Rest> rests = new ArrayList<Rest>();
     private final String keyId = "9ffa01190536dce72adf62e5fba762be";
     private final String format = "json";
 
     static EditText editText;
-    static TextView textView;
+
+    //コネクタ
+    GurunaviConnect connect = new GurunaviRetrofit();
+
+    final Gurunavi gurunavi = new Gurunavi();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_main);
 
+        listView = (ListView)findViewById(R.id.listView);
         editText = (EditText)findViewById(R.id.editText);
-        textView = (TextView)findViewById(R.id.textView);
 
+    }
+
+    public void search(View v) {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(REQUEST_DOMAIN)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        GurunaviService service = retrofit.create(GurunaviService.class);
+        GurunaviRetrofit.GurunaviApiService service = retrofit.create(GurunaviRetrofit.GurunaviApiService.class);
 
-        Call<GurunaviResponse> call = service.search(keyId, format, "東京");
+        Call<GurunaviResponse> call = service.search(keyId, format, editText.getText().toString());
+        Log.d("TAG", "RETROFIT");
         call.enqueue(new Callback<GurunaviResponse>() {
             @Override
             public void onResponse(Response<GurunaviResponse> response, Retrofit retrofit) {
                 Log.d("TAG", "GurunaviRetrofit_Response");
                 //listener.onSuccess(gurunaviResponse);
-
-                List<Rest> restList = response.body().getApiVersion().getRestList();
-                StringBuilder builder = new StringBuilder();
-
-                for (Rest rest : restList) {
-                    builder.append(rest.getName() + "\n");
-                }
-                textView.setText(new String(builder));
             }
 
             @Override
@@ -76,25 +74,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-
-//    public void search(View v) {
-//
-//
 //        gurunavi.search(connect, keyId, format, editText.getText().toString(), new GurunaviConnect.GurunaviSearchListener() {
 //
 //            @Override
-//            public void onSuccess(List<Rest> restList) {
+//            public void onSuccess(GurunaviResponse gurunaviResponse) {
 //                //とりだす
-//                ArrayList<Rest> rests = new ArrayList<>();
-//
-//                for (Rest d : restList) {
-//                    rests.add(d);
-//                }
-//
-//                adapter = new CustomAdapter(getApplicationContext(), R.layout.activity_main, rests);
-//                listView.setAdapter(adapter);
+////                ArrayList<Rest> rests = new ArrayList<>();
+////
+////                for (Rest d : restList) {
+////                    rests.add(d);
+////                }
+////
+////                adapter = new CustomAdapter(getApplicationContext(), R.layout.activity_main, rests);
+////                listView.setAdapter(adapter);
 //
 //                Log.d("TAG", "success!!!!!");
 //
@@ -106,8 +98,8 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //
 //        });
-//
-//
-//        //listView.setAdapter(adapter);
-//    }
+
+
+        //listView.setAdapter(adapter);
+    }
 }
